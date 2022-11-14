586f65ccf0a2833333a10cbb0e5d2ded604df87d
zhaolinxu <zhaolinxu@sina.cn>
2022-11-14 09:50:04 +0800

    fix

--- worker.js
+++ worker.js
@@ -1780,6 +1780,9 @@
         }
 
         static process(dT) {
+            if(CreatureBasic.numCreatures < 0) {
+                CreatureBasic.numCreatures = 0;
+            }
             BasicResources.subtract('energy', CreatureBasic.getEnergyConsumption()
                 * dT);
             if(BasicResources.resources.energy <= 0 && CreatureBasic.numCreatures > 0) {
@@ -2003,7 +2006,7 @@
             hpBonus *= (1 + BasicHeirlooms.getTotalBonus('resilience'));
             let dmgBonus = 1 + 0.25*BasicResearch.getResearchLevel('fighting');
             dmgBonus *= Math.pow(1.1, BasicBuilding.getBuildingLevel('zeusStatue'));
-            dmgBonus *= (1 + BasicHeirlooms.getTotalBonus('aggression'));
+            dmgBonus *= (1 + BasicHeirlooms.getTotalBonus('agression'));
             const qt = CreatureJobs.getWorkerAmount('fighter');
             let totMult = 1.0;
             if(BasicTemper.getCurrentTemper() === 'aggressive') {
@@ -2157,7 +2160,7 @@
         getText: (amount) => `+${fmtVal(amount*100)}% to maximum mana`
     },{
         id: 'agression',
-        name: 'agression',
+        name: 'aggression',
         isUnlocked: () => true,
         base: 0.01,
         getText: (amount) => `+${fmtVal(amount*100)}% to creatures attack`
